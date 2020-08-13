f1dc98f6402fa1fabe26366839c895d0457fa478
Roy <38908560+15250969591@users.noreply.github.com>
2020-08-13 10:34:17 +0800

    上传测试
    第一个上传测试

---
+++ SmsHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gas.Forms.Common.SmsHelper
+{
+    public class SmsHelper
+    {
+        /// <summary>
+        /// POST短信发送
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public string HttpPostMsg(string param, string postUrl)
+        {
+            Encoding encoding = Encoding.GetEncoding("UTF-8");
+            byte[] bs = Encoding.UTF8.GetBytes(param);
+            var request = (HttpWebRequest)HttpWebRequest.Create(postUrl);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded;charset=UTF-8";
+            request.ContentLength = bs.Length;
+            using (Stream stream = request.GetRequestStream())
+            {
+                stream.Write(bs, 0, bs.Length);
+                stream.Close();
+            }
+
+            WebResponse webRequest = request.GetResponse();
+            StreamReader streamReader = new StreamReader(webRequest.GetResponseStream(), encoding);
+            string strResult = streamReader.ReadToEnd().Trim();
+            webRequest.Close();
+            streamReader.Close();
+
+            return strResult;
+        }
+        /// <summary>
+        /// POST短信发送
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public string HttpPostCivilMeter(string param, string postUrl)
+        {
+            Encoding encoding = Encoding.GetEncoding("UTF-8");
+            byte[] bs = Encoding.UTF8.GetBytes(param);
+            var request = (HttpWebRequest)HttpWebRequest.Create(postUrl);
+            request.Method = "POST";
+            request.ContentType = "application/json;charset=UTF-8";
+            request.ContentLength = bs.Length;
+            
+            using (Stream stream = request.GetRequestStream())
+            {
+                stream.Write(bs, 0, bs.Length);
+                stream.Close();
+            }
+
+            WebResponse webRequest = request.GetResponse();
+            StreamReader streamReader = new StreamReader(webRequest.GetResponseStream(), encoding);
+            string strResult = streamReader.ReadToEnd();
+            webRequest.Close();
+            streamReader.Close();
+
+            return strResult;
+        }
+
+    }
+}
---
+++ ValidateCodeHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gas.Forms.Common.SmsHelper
+{
+    public class ValidateCodeHelper
+    {
+        /// <summary>
+        /// 验证码的最大长度
+        /// </summary>
+        public int MaxLength
+        {
+            get { return 6; }
+        }
+        /// <summary>
+        /// 验证码的最小长度
+        /// </summary>
+        public int MinLength
+        {
+            get { return 6; }
+        }
+
+
+        public string CreateValidateCode(int length)
+        {
+            int[] randMembrs = new int[length];
+            int[] validateNums = new int[length];
+            string validateNumberStr = "";
+            //生成起始序列号
+            int seekSeek = unchecked((int)DateTime.Now.Ticks);
+            Random seekRand = new Random(seekSeek);
+            int beginSeek = (int)seekRand.Next(0, Int32.MaxValue - length * 10000);
+            int[] seeks = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                beginSeek += 10000;
+                seeks[i] = beginSeek;
+            }
+            //生成随机数
+            for (int i = 0; i < length; i++)
+            {
+                Random rand = new Random(seeks[i]);
+                int pownum = 1 * (int)Math.Pow(10,length);
+                randMembrs[i] = rand.Next(pownum,Int32.MaxValue);
+            }
+            //抽取随机数字
+            for (int i = 0; i < length; i++)
+            {
+                string numStr = randMembrs[i].ToString();
+                int numLength = numStr.Length;
+                Random rand = new Random();
+                int numPosition = rand.Next(0,numLength-1);
+                validateNums[i] = Int32.Parse(numStr.Substring(numPosition,1));
+            }
+            //生成验证码
+            for (int i = 0; i < length; i++)
+            {
+                validateNumberStr += validateNums[i].ToString();
+            }
+            return validateNumberStr;
+
+        }
+    }
+}
